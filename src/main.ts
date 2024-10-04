1032dd9b04cfbfa28dca25c1ab6ee21676244db2
PricedTuna <edgarevegar04@gmail.com>
2024-10-03 19:05:11 -0700

    Add swagger to documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('RRHH backend')
+    .setDescription('API REST for Banchido project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  let port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
