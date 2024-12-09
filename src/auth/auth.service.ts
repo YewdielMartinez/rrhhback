b9b4d5ab105fd204e8cf135d3dc43ea954a55dc1
PricedTuna <edgarevegar04@gmail.com>
2024-12-08 18:55:30 -0700

    add JWT and .ENV

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,35 +1,33 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsuarioService } from 'src/usuario/usuario.service';
 import { LogInDto } from './dto/LogInDto';
 import { SignUpUserDto } from './dto/sign-up-user.dto';
-import { UsuarioLogIn } from './dto/usuario-log-in.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsuarioService) {}
+  constructor(
+    private usuarioService: UsuarioService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async logIn(logInDto: LogInDto) {
-    try {
-      const user = await this.usersService.findByEmail(logInDto.correo);
+    const user = await this.usuarioService.findByEmail(logInDto.correo);
 
-      if (user.password !== logInDto.password) {
-        throw new UnauthorizedException();
-      }
-
-      const { password, ...usuarioSinPassword } = user;
-      const usuarioLogIn: UsuarioLogIn = usuarioSinPassword as UsuarioLogIn;
-
-      return usuarioLogIn
-    } catch (error) {
-      return {
-        message: 'error ocurred',
-        responseCode: 500,
-        result: null,
-      };
+    if (!user || user.password !== logInDto.password) {
+      throw new UnauthorizedException('Invalid credentials');
     }
+
+    const payload = { email: user.correo, sub: user.idUsuario }; // Datos a incluir en el token
+    const token = this.jwtService.sign(payload);
+
+    return {
+      accessToken: token,
+      user: { ...user, password: undefined }, // Excluir el password del objeto retornado
+    };
   }
 
   async signUp(signUpUserDto: SignUpUserDto) {
-    return this.usersService.create(signUpUserDto);
+    return this.usuarioService.create(signUpUserDto);
   }
 }
